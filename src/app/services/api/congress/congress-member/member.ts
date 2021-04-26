dff03bd2764d5b1147ea2e02e9c44a51522072cf
Juan Cruz Sadone <juancupp@hotmail.com>
2021-04-26 00:50:38 -0300

    Modified member interface

--- src/app/services/api/congress/congress-member/member.ts
+++ src/app/services/api/congress/congress-member/member.ts
@@ -1,12 +1,17 @@
+export interface IResult {
+    results: IMember[];
+}
+
 export interface IMember {
     id: string;
     title: string;
     short_tittle: string;
     first_name: string;
     last_name: string;
-    date_of_birth: Date;
+    date_of_birth: string;
     gender: string;
     party: string;
     total_votes: number;
     phone: string;
 }
+
